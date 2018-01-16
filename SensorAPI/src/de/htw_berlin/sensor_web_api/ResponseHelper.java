ef72acbb11038705dda47a5207f6d44cdfdf735f
Benny Lach <benny@kptncook.com>
2018-01-17 00:36:07 +0100

    Added method to handle correct create requests

--- SensorAPI/src/de/htw_berlin/sensor_web_api/ResponseHelper.java
+++ SensorAPI/src/de/htw_berlin/sensor_web_api/ResponseHelper.java
@@ -1,7 +1,6 @@
 package de.htw_berlin.sensor_web_api;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ResponseHelper {
@@ -9,10 +8,14 @@ public class ResponseHelper {
     /**
      * Method to handle wrong or unknown requests
      *
-     * @param resp the respsonse object
-     * @throws IOException if something went wrong sending the response
+     * @param resp the response object
+     * @throws Exception if the response object is null or something went wrong sending the response
      */
-    static void handleWrongRequest(HttpServletResponse resp) throws IOException {
+    static void handleWrongRequest(HttpServletResponse resp) throws Exception {
+        if (resp == null) {
+            throw new Exception("The HttpServletResponse object must not be null!");
+        }
+
         resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
@@ -23,17 +26,42 @@ public class ResponseHelper {
     }
 
     /**
-     * Method to handle valid requests
+     * Method to handle a valid create request
+     *
      * @param resp the response object
-     * @throws IOException If something went wrong sending the response
+     * @param id the id of the created object
+     * @throws Exception if one param is null or something went wrong sending the response
      */
-    static void handleValidRequest(HttpServletResponse resp) throws IOException {
+    static void handleValidCreateRequest(HttpServletResponse resp, String id) throws Exception {
+        if (id == null || resp == null) {
+            throw new Exception("One or more parameters are not valid");
+        }
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.append("{\"id\": \"" + id + "\"}");
+        writer.close();
+    }
+
+    /**
+     * Method to handle valid requests
+     *
+     * @param resp the response object
+     * @throws Exception if the response object is null or something went wrong sending the response
+     */
+    static void handleValidRequest(HttpServletResponse resp) throws Exception {
+        if (resp == null) {
+            throw new Exception("The HttpServletResponse object must not be null!");
+        }
+
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
         PrintWriter writer = resp.getWriter();
-        writer.append("{\"success\"}");
+        writer.append("{\"status\": \"success\"}");
         writer.close();
     }
 }
