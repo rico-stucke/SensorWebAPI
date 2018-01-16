1c25440e4605bd7208f8538b20bdddc5780cf450
Benny Lach <benny@kptncook.com>
2018-01-17 00:26:42 +0100

    Added Helper for handling JSON Strings

---
+++ SensorAPI/src/de/htw_berlin/sensor_web_api/helper/JSONParser.java
@@ -0,0 +1,61 @@
+package de.htw_berlin.sensor_web_api.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Benny Lach
+ *
+ * JSON Parser
+ */
+public class JSONParser {
+
+    /**
+     * Method to parse a given json string to Map<String,String> Object if possible
+     * @param input the input String
+     * @return Mapped Object - will be null if something went wrong
+     */
+    public static Map<String,String> parse(String input) {
+        if (input == null) { return null; }
+
+        return JSONParser.convert(input);
+    }
+
+    /**
+     * Method to remove unnecessary chars from the given json input string
+     *
+     * @param input json string
+     */
+    private static String reduce(String input) {
+        input = input
+                .replace("{", "")
+                .replaceAll("}", "")
+                .replaceAll("\"", "");
+
+        return input;
+    }
+
+    /**
+     * Method to convert a given String into a corresponding Map Representation
+     *
+     * @param input json String to convert
+     * @return Map representation - will be null if something went wrong mapping input
+     */
+    private static Map<String, String> convert(String input) {
+        input = JSONParser.reduce(input);
+
+        HashMap<String, String> map = new HashMap<>();
+
+        System.out.println("Reduced json string: " + input);
+        String[] pairs = input.split(",");
+
+        for (String pair : pairs) {
+            String[] kv = pair.split(":");
+            if (kv.length != 2) {
+                return null;
+            }
+            map.put(kv[0], kv[1]);
+        }
+        return map;
+    }
+}
